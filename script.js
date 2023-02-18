97671a6af8a105e927bc481b4a78104e1c90ace2
wisamosman <viesam_1987@hotmail.com>
2023-02-18 21:54:03 +0100

    add html23

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const todoInput = document.querySelector('.todo-input')
 const todoBtn = document.querySelector('.todo-btn')
 function addTodo(e){
     e.preventDefault();
-console.log('add todo')
+console.log(todoInput.value)
+todoInput.value= ''
 }
 todoBtn.addEventListener('click' ,addTodo)
