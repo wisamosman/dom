cb3be9babedc94073352751acb3a19d9ee621f30
wisamosman <viesam_1987@hotmail.com>
2023-02-18 21:32:17 +0100

    add html21

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 console.log('welcom');
 console.log('ana wesa')
 const todoInput = document.querySelector('.todo-input')
-const todoBtn = document.querySelector('todo-btn')
+const todoBtn = document.querySelector('.todo-btn')
+function addTodo(){
+console.log('addTodo')
+}
