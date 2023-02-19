c2a0b1b3b44b4d6b3ca32ec46f73af06b7487f8c
wisamosman <viesam_1987@hotmail.com>
2023-02-19 10:59:58 +0100

    add html26

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ console.log('welcom');
 console.log('ana wesa')
 const todoInput = document.querySelector('.todo-input')
 const todoBtn = document.querySelector('.todo-btn')
+const todoResult = document.querySelector('.todo-result')
 function addTodo(e){
     e.preventDefault();
 console.log(todoInput.value)
