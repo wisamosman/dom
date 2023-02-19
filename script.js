4dd37637d2bcd26acb7ffb73ea09c09e895dddfe
wisamosman <viesam_1987@hotmail.com>
2023-02-19 11:02:39 +0100

    add html27

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const todoBtn = document.querySelector('.todo-btn')
 const todoResult = document.querySelector('.todo-result')
 function addTodo(e){
     e.preventDefault();
+    const todoLi = document.createElement("li")
 console.log(todoInput.value)
 todoInput.value= ''
 }
